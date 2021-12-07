71c1043fea77b30a2eba7adbeef74c2f1497a7c0
tourtiere <leonard-dufour@hotmail.com>
2021-12-07 10:18:22 -0500

    day 7! :santa:

--- day7/index.py
+++ day7/index.py
@@ -1,2 +1,18 @@
 pos = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
-print(sum(pos) / len(pos))
+
+
+def fuel(position, idx):
+    n = abs(position - idx)
+    # problem a
+    # return n
+
+    # problem b
+    return n * (n+1) / 2
+
+
+m = min([
+    sum([fuel(p, i) for p in pos])
+    for i in range(len(pos))
+])
+
+print(m)
