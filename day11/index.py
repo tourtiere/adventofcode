5866cd98206c903f83a3a82efa2bca06e700d98d
tourtiere <leonard-dufour@hotmail.com>
2021-12-11 00:51:09 -0500

    ouin

--- day11/index.py
+++ day11/index.py
@@ -7,36 +7,32 @@ with open("./data.txt") as f:
     }
 
 
-neighbours = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (1, -1), (1, 1), (-1, 1)]
-
-
 def flash(flashes: list[tuple[int, int]]):
     global graph
     new_flashes = [key for key in graph.keys() if graph[key] > 9 and key not in flashes]
     flashes += new_flashes
 
     for flash_idx in new_flashes:
-        for n in neighbours:
-            n_idx = (flash_idx[0] + n[0], flash_idx[1] + n[1])
-            if n_idx in graph:
-                graph[n_idx] += 1
+        for delta in [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (1, -1), (1, 1), (-1, 1)]:
+            n = (flash_idx[0] + delta[0], flash_idx[1] + delta[1])
+            if n in graph:
+                graph[n] += 1
         flashes = flash(flashes)
 
     return flashes
 
 
 flash_count = 0
-for i in range(100):
-    graph = {
-        idx: data + 1
-        for idx, data in graph.items()
-    }
+for i in range(1, 2555):
+    for idx in graph:
+        graph[idx] += 1
+
     flashes = flash([])
     for f in flashes:
         graph[f] = 0
     flash_count += len(flashes)
 
     if (len(flashes) == len(graph.keys())):
-        print("breakcount", i+1)
+        print("all", i)
         break
 print()
