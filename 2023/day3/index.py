0873fe7f99ff3bc318579605384ae1d59392a17a
tourtiere <leonard-dufour@hotmail.com>
2023-12-03 15:42:31 -0500

    save

--- 2023/day3/index.py
+++ 2023/day3/index.py
@@ -1,50 +1,33 @@
-import re
-import math
-
-
-content = '''467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..'''
 with open("./data.txt") as f:
     content = f.read()
     lines = content.split("\n")
-    grid = { (i, j):c for j,line in enumerate(lines) for i,c in enumerate(line)}
+    grid = { (i, j):c for j, line in enumerate(lines) for i, c in enumerate(line)}
     s1 = 0
 
     gears = {k:[] for k in grid}
 
     for j, line in enumerate(lines):
         number = ""
-        for i in range(len(line)):
-            letter = line[i]
-            succ = grid.get((i+1, j))
-            if (letter.isdigit()):
-                number += letter
-                if succ is None or not succ.isdigit():
-                    neighbors = [(i+1-k, j-1) for k in range(len(number)+2) ]
-                    neighbors += [(i+1-k, j+1) for k in range(len(number)+2) ]
-                    neighbors += [(i+1, j), (i - len(number), j)]
-                    values = [grid.get(n) for n in neighbors] 
-                    seul = all([v is None or v == "." for v in values ])
-                    
-                    if not seul:
-                        s1 +=int(number)
-
-                    for n,value in zip(neighbors, values):
-                        if (value=="*"):
-                            gears[n].append(int(number))
-
-            else:
+        for i, letter in enumerate(line):
+            if not letter.isdigit():
                 number = ""
+                continue
+            number += letter
+            succ = grid.get((i+1, j))
+            if succ is None or not succ.isdigit():
+                adj_keys = [(i+1-k, j-1) for k in range(len(number)+2) ]
+                adj_keys += [(i+1-k, j+1) for k in range(len(number)+2) ]
+                adj_keys += [(i+1, j), (i - len(number), j)]
+                adj_values = [grid.get(n) for n in adj_keys] 
+                alone = all([v is None or v == "." for v in adj_values ])
+
+                if not alone:
+                    s1 +=int(number)
+
+                for key, value in zip(adj_keys, adj_values):
+                        if (value=="*"):
+                            gears[key].append(int(number))
+
     gear_ratios = sum([gears[g][0]*gears[g][1] for g in gears if len(gears[g]) ==2])
     print(s1)
     print(gear_ratios)
-
-    
