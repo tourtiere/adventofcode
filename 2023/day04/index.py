5e7e6dbedb6d24747013fbfa977fd2ee4b136339
tourtiere <leonard-dufour@hotmail.com>
2023-12-04 15:46:54 -0500

    refactor

--- 2023/day04/index.py
+++ 2023/day04/index.py
@@ -1,16 +1,16 @@
-import re 
+from collections import defaultdict
 
 with open("./data.txt") as f:
-    content =f.read() 
+    content = f.read() 
     lines = content.split("\n")
-    part1=0
-    part2=0
-    scratches = {i:1 for i,_ in enumerate(lines)}
+    part1 = 0
+    scratches = defaultdict(int)
     R = len(lines)
     for i, line in enumerate(lines):
+        scratches[i] += 1
         numbers = line.split(":")[1].split("|")
-        winning, hand = [[int(i) for i in re.findall(r"\d+", s)]  for s in numbers]
-        score= 0
+        winning, hand = [[int(i) for i in s.split()]  for s in numbers]
+        score = 0
         matches = [n for n in hand if n in winning]
         if (len(matches) != 0):
             score =2**(len(matches)-1)
@@ -18,8 +18,8 @@ with open("./data.txt") as f:
         for j in range(len(matches)):
             if i+j+1 < R:
                 scratches[i+j+1] += scratches[i] 
-        part2 += scratches[i]
         part1 += score
         
     print(part1)
-    print(part2)
+    print(sum(scratches.values()))
+
