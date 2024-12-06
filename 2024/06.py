ec98fb95e6dbc939bc5de342c36ad5a8686b7f6c
tourtiere <tourtiere@users.noreply.github.com>
2024-12-06 10:31:01 -0500

    :christmas_tree:

--- 2024/06.py
+++ 2024/06.py
@@ -8,27 +8,27 @@ def compute(pos:tuple[int,int] )-> int | None:
     visited = set()
     states = set() # for part 2
     while grid.get(pos):
-        state = tuple((pos[0],pos[1], dir_idx))
-        if state in states: return None
+        state = tuple((pos[0], pos[1], dir_idx))
+        if state in states:
+            return None
         states.add(state)
         visited.add(pos)
         dx, dy = directions[dir_idx]
-        next_pos = pos[0]+ dx, + pos[1] + dy
+        next_pos = pos[0] + dx, + pos[1] + dy
         if grid.get(next_pos) == '#':
-            dir_idx  = (dir_idx + 1) % 4
-            dx, dy = directions[dir_idx]
+            dir_idx = (dir_idx + 1) % 4
         else:
             pos = next_pos
     return len(visited)
 
-init_pos = [pos for pos,c in grid.items() if c == '^'][0]
+init_pos = [pos for pos, c in grid.items() if c == '^'][0]
 print(compute(init_pos)) # part1
 part2 = 0
-for pos,c in grid.items():
+for pos, c in grid.items():
+    print(pos)
     if c == ".": 
         grid[pos] = "#"
         if compute(init_pos) is None:
             part2 +=1
         grid[pos] = '.'
 print(part2)
-
