33b981cdf4c087d3d2b7d0fc57660abb2809066f
tourtiere <leonard-dufour@hotmail.com>
2024-12-12 11:41:06 -0500

    :santa:

--- 2024/12.py
+++ 2024/12.py
@@ -1,22 +1,18 @@
 content = open('data.txt').read()
 lines = content.split('\n')
 
-def adjacents(p: tuple[int,int]):
-    return [ (delta, (p[0]+delta[0], p[1]+delta[1])) for delta in [(-1,0), (0,-1), (1,0), (0,1)] ]
-
 grid = {(i,j):lines[j][i] for j in range(len(lines)) for i in range(len(lines[0]))}
 
-def flood_fill(pos: tuple[int, int], group:set, fences:list, sides_count=0):
-    sides = 0
-    for delta, p in adjacents(pos):
-        if p not in group and grid.get(p) == grid[pos]:
+def flood_fill(pos: tuple[int, int], group:set, fences:list ):
+    x, y = pos
+    for dx, dy in [(-1,0), (0,-1), (1,0), (0,1)] :
+        p = x + dx, y + dy
+        if p in group: continue
+        if grid[pos] == grid.get(p):
             group.add(p)
-            sides += flood_fill(p, group, fences)
-        elif grid.get(p) != grid[pos]:
-            # [ for a,b in [(-1,0), (0,-1), (1,0), (0,1)]]
-            fence = tuple([*pos, *delta])
-            fences.append(fence)
-    return sides + sides_count
+            flood_fill(p, group, fences)
+        else:
+            fences.append((x,y, dx,dy))
 
 def count_sides(fences):
     count = 0
